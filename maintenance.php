deaf0d2ebf46cb242990c8733a260a24c72f9e93
erwantanguy <erwan@ticoet.me>
2018-02-18 17:00:29 +0100

    mise en place de la page de maintenance
    avec font awesome en sass

---
+++ maintenance.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="fr-FR" prefix="og: http://ogp.me/ns#">
+    <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+            <title>
+                    Polybrid Production            </title>
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="pingback" href="http://polybridprod.fr/xmlrpc.php">
+            <script src="http://polybridprod.fr/wp-content/themes/polybridprod/js/jquery.min.js"></script>
+                <?php $date = new DateTime();?><meta data-test="<?php echo $date->format('His'); ?>" >           
+<link rel='stylesheet' id="contact-form-7-css"  href="http://polybridprod.fr/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=<?php echo $date->format('His');?>" type="text/css" media="all" />
+<link rel="stylesheet" id="style-css"  href="http://polybridprod.fr/wp-content/themes/polybridprod/css/style.css?ver=<?php echo $date->format('His');?>" type="text/css" media="all" />
+<style>.post-thumbnail img[src$='.svg'] { width: 100%; height: auto; }</style>
+<link rel="icon" href="http://polybridprod.fr/wp-content/uploads/2018/02/cropped-favicon-32x32.png" sizes="32x32" />
+<link rel="icon" href="http://polybridprod.fr/wp-content/uploads/2018/02/cropped-favicon-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="http://polybridprod.fr/wp-content/uploads/2018/02/cropped-favicon-180x180.png" />
+<meta name="msapplication-TileImage" content="http://polybridprod.fr/wp-content/uploads/2018/02/cropped-favicon-270x270.png" />
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-114195314-1', 'auto');
+  ga('send', 'pageview');
+</script>
+    </head>
+
+    <body class="maintenance container">
+        <div id="header" class="row wpm_social"><ul class="wpm_horizontal col-sm"><li><a href="https://www.facebook.com/polybrid.polybrid/" target="_blank"><img src="http://polybridprod.fr/wp-content/plugins/wp-maintenance/socialicons/style3/32/facebook.png" alt=" Facebook" title=" Facebook"></a></li><li><a href="https://twitter.com/_Polybrid_" target="_blank"><img src="http://polybridprod.fr/wp-content/plugins/wp-maintenance/socialicons/style3/32/twitter.png" alt=" Twitter" title=" Twitter"></a></li><li><a href="https://www.linkedin.com/company/polybrid-production/" target="_blank"><img src="http://polybridprod.fr/wp-content/plugins/wp-maintenance/socialicons/style3/32/linkedin.png" alt=" Linkedin" title=" Linkedin"></a></li><li><a href="https://www.youtube.com/channel/UCTlF1CbBrNUr_jTVYaFNrYw" target="_blank"><img src="http://polybridprod.fr/wp-content/plugins/wp-maintenance/socialicons/style3/32/youtube.png" alt=" Youtube" title=" Youtube"></a></li></ul></div>
+        <header class="row">
+            <div class="col-sm">
+                <h1><?php bloginfo('name'); ?></h1>
+                <h2><?php bloginfo('description'); ?></h2>
+            </div>
+        </header>
+        <section class="row justify-content-sm-center">
+            <figure class="col-sm-3">
+                <img src="http://polybridprod.fr/wp-content/uploads/2018/02/logo.jpg" alt="logo Polybrid Production">
+            </figure>
+            <nav id="socialmedia" class="col-sm-auto">
+                <ul>
+                    <li><a href="https://www.facebook.com/polybrid.polybrid/"><i class="fab fa-facebook"></i> Polybrid Production est sur Facebook</a></li>
+                    <li><a href="https://www.instagram.com/polybrid/"><i class="fab fa-instagram"></i> Polybrid Production est sur Instagram</a></li>
+                    <li><a href="https://www.instagram.com/laerosol_paris/?hl=fr"><i class="fab fa-instagram"></i> L'Aérosol est sur Instagram</a></li>
+                </ul>
+            </nav>
+        </section>
+        <section class="row justify-content-sm-center">
+            <iframe src="https://www.youtube.com/embed/9xfLEfIZ_oQ" width="560" height="315" frameborder="0" allowfullscreen=""></iframe>
+        </section>
+        <section id="content" class="row">
+            <aside class="col-sm-5">
+                <h3>Nos événements</h3>
+                <ul>
+                    <li><a href="https://www.facebook.com/Roule-Des-Patins-1070696526397165/﻿"><img src="http://polybridprod.fr/wp-content/uploads/2018/02/PATINS-NOIR-1.svg" alt="Logo Roule des Patins"></a></li>
+                    <li><a href="https://www.facebook.com/festivalparismusic/"><img src="http://polybridprod.fr/wp-content/uploads/2018/02/paris-music-2.svg" alt="logo festival Paris Music"></a></li>
+                    <li><a href="https://www.facebook.com/Rocknrolls-NOT-DEAD-503789889727607/"><img src="http://polybridprod.fr/wp-content/uploads/2018/02/LOGO_RockIsNotDead.jpg" alt="LOGO Rock Is Not Dead"></a></li>
+                    <li><a href="https://www.facebook.com/3672TECHNO/photos/a.668029896608958.1073741827.666639156748032/1114521261959817/?type=1&theater"><img src="http://polybridprod.fr/wp-content/uploads/2018/02/3672-techno-4.svg" alt="logo 3672 Techno"></a></li>
+                </ul>
+                <p>
+                    Pour info et programmation > <a href="mailto:info@polybridprod.com">info@polybridprod.com</a>
+                </p>
+            </aside>
+            <main class="col-sm-7">
+                <h3>Notre lieu</h3>
+                <div class="row justify-content-end">
+                    <figure class="col-sm-6">
+                        <a href="http://www.laerosol.fr/"><img src="http://polybridprod.fr/wp-content/uploads/2018/02/lAerosol-nb.jpg" alt="logo Aérosol"></a>
+                    </figure>
+                </div>
+                <p>
+                    Pour privatiser > <a href="mailto:sandra@polybridprod.com">sandra@polybridprod.com</a>
+                </p>
+                <p>
+                    Pour info et programmation > <a href="mailto:info@polybridprod.com">info@polybridprod.com</a>
+                </p>
+            </main>
+        </section>
+<script type='text/javascript' src='http://polybridprod.fr/wp-content/themes/polybridprod/bundle.js?ver=4.9.4'></script>    
+</body>
+</html>
